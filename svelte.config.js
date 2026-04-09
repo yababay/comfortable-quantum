d3c464459309cb4a86d26fecc231c285eb0feebe
Михаил Беляков <bigbelk@live.ru>
2026-04-09 15:56:18 +0300

    gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -16,6 +16,9 @@ const config = {
 			precompress: false,
 			strict: true
 		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/comfortable-quantum' : '',
+		},
 		prerender: {
 			handleHttpError: 'warn'
 		}
